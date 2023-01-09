1b3dd5510e505ada73069b8a5307dd5ea052a0ce
shawn.wang <shawn.wang86b@gmail.com>
2023-01-09 23:42:11 +1100

    add switch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
-import { Route, Redirect } from "react-router-dom";
+import { Route, Redirect, Switch } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import ChatPage from "./Pages/ChatPage";
 
 function App() {
   return (
     <div className="App">
-      <Route path="/" component={HomePage} exact />
-      <Route path="/chats" component={ChatPage} />
-      <Redirect from="*" to="/" />
+      <Switch>
+        <Route path="/" component={HomePage} exact />
+        <Route path="/chats" component={ChatPage} />
+        <Redirect from="*" to="/" />
+      </Switch>
     </div>
   );
 }
